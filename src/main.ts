3aa6d5c5a4c6e01d5287bc6b312dbbc238c5446f
空巷一人 <1530688385@qq.com>
2024-07-19 12:18:43 +0800

    添加robots.txt

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.set('trust proxy', 1);
   app.disable('x-powered-by');
+  app.useStaticAssets('public');
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   await app.listen(process.env.PORT || 7860, '0.0.0.0').then(async () => {
