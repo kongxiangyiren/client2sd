453f9a32752049a53734cf60369bfaff390dad8e
空巷一人 <1530688385@qq.com>
2024-07-29 12:33:29 +0800

    fix

--- ncc.cp.js
+++ ncc.cp.js
@@ -11,6 +11,29 @@ nsfwjsApi.cpModel();
 copyFileSync('./.env', './build/.env');
 
 // 删除多余
-// rmSync('./build/build/deps', { recursive: true, force: true });
-// rmSync('./build/build/lib/napi-v9', { recursive: true, force: true });
+if (process.platform === 'linux') {
+  rmSync('./build/build/deps/lib/libtensorflow.so.2.9.1', {
+    recursive: true,
+    force: true,
+  });
+
+  rmSync('./build/build/deps/lib/libtensorflow_framework.so.2.9.1', {
+    recursive: true,
+    force: true,
+  });
+} else if (process.platform === 'win32') {
+  rmSync('./build/build/deps', { recursive: true, force: true });
+} else if (process.platform === 'darwin') {
+  rmSync('./build/build/deps/lib/libtensorflow.2.7.0.dylib', {
+    recursive: true,
+    force: true,
+  });
+
+  rmSync('./build/build/deps/lib/libtensorflow_framework.2.7.0.dylib', {
+    recursive: true,
+    force: true,
+  });
+}
+
+rmSync('./build/build/lib/napi-v9', { recursive: true, force: true });
 rmSync('./build/build/prebuilds', { recursive: true, force: true });
