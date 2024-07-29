2e5a52bdcf38c8e455f5d333920dd04c735c2ebd
空巷一人 <1530688385@qq.com>
2024-07-29 14:29:54 +0800

    测试

--- ncc.cp.js
+++ ncc.cp.js
@@ -1,6 +1,6 @@
 const nsfwjsApi = require('nsfwjs-api');
 const { join } = require('path');
-const { rmSync, cpSync } = require('fs');
+const { rmSync, cpSync, unlinkSync } = require('fs');
 nsfwjsApi.UseModel = true;
 nsfwjsApi.model = join(process.cwd(), 'build/model');
 
@@ -16,27 +16,14 @@ cpSync('./public', './build/public', {
 
 // 删除多余
 if (process.platform === 'linux') {
-  rmSync('./build/build/deps/lib/libtensorflow.so.2.9.1', {
-    recursive: true,
-    force: true,
-  });
-
-  rmSync('./build/build/deps/lib/libtensorflow_framework.so.2.9.1', {
-    recursive: true,
-    force: true,
-  });
+  unlinkSync('./build/build/deps/lib/libtensorflow.so.2.9.1');
+  unlinkSync('./build/build/deps/lib/libtensorflow_framework.so.2.9.1');
 } else if (process.platform === 'win32') {
   rmSync('./build/build/deps', { recursive: true, force: true });
 } else if (process.platform === 'darwin') {
-  rmSync('./build/build/deps/lib/libtensorflow.2.7.0.dylib', {
-    recursive: true,
-    force: true,
-  });
+  unlinkSync('./build/build/deps/lib/libtensorflow.2.7.0.dylib');
 
-  rmSync('./build/build/deps/lib/libtensorflow_framework.2.7.0.dylib', {
-    recursive: true,
-    force: true,
-  });
+  unlinkSync('./build/build/deps/lib/libtensorflow_framework.2.7.0.dylib');
 }
 
 rmSync('./build/build/lib/napi-v9', { recursive: true, force: true });
