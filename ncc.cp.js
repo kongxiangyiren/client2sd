25f374426618bc94c88f5b3e08e9cdd4f73181c7
空巷一人 <1530688385@qq.com>
2024-07-29 11:00:40 +0800

    鉴图版添加可执行文件打包

---
+++ ncc.cp.js
@@ -0,0 +1,16 @@
+const nsfwjsApi = require('nsfwjs-api');
+const { join } = require('path');
+const { copyFileSync, rmSync } = require('fs');
+nsfwjsApi.UseModel = true;
+nsfwjsApi.model = join(process.cwd(), 'build/model');
+
+//   copy模型文件夹, UseModel为false时无效
+// 模型文件 https://github.com/infinitered/nsfwjs/tree/master/models/inception_v3
+nsfwjsApi.cpModel();
+
+copyFileSync('./.env', './build/.env');
+
+// 删除多余
+rmSync('./build/build/deps', { recursive: true, force: true });
+rmSync('./build/build/lib/napi-v9', { recursive: true, force: true });
+rmSync('./build/build/prebuilds', { recursive: true, force: true });
